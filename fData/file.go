6a86ca90516f0594e3216301fa1d4bf221b3610e
何其龙 <heqilong.hl@bytedance.com>
2025-03-07 15:22:19 +0800

    feat：添加WriteToFileIfExists函数

---
+++ fData/file.go
@@ -0,0 +1,39 @@
+package fData
+
+import (
+	"os"
+)
+
+// WriteToFileIfExists 写入文件
+func WriteToFileIfExists(
+	filePath string,
+	content string,
+) {
+	// 检查文件是否存在
+	_, err := os.Stat(filePath)
+	if os.IsNotExist(err) {
+		// 创建文件
+		_, err := os.Create(filePath)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// 如果文件存在，打开文件以追加模式写入内容
+	file, err := os.OpenFile(
+		filePath,
+		os.O_WRONLY|os.O_CREATE,
+		0644,
+	)
+	if err != nil {
+		return
+	}
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
+
+	_, err = file.WriteString(content)
+	if err != nil {
+		return
+	}
+}
