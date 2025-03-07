ffac03c5b0ee2f22e48eb49e132baf151511f2dd
何其龙 <heqilong.hl@bytedance.com>
2025-03-07 17:49:45 +0800

    feat：添加注释

--- test/fData_test.go
+++ test/fData_test.go
@@ -3,17 +3,11 @@ package test
 import (
 	"log"
 	"testing"
-
-	"github.com/zhenjunhl/fa-tian-xiang-di/f"
 )
 
 func TestGetTimestampHoursAgo(t *testing.T) {
 	// ago := f.GetTimestampHoursAgo(1)
 	// fmt.Println(ago)
-}
-func TestMergeAndDeduplicateStrings(t *testing.T) {
-	strings := f.MergeAndDeduplicateStrings([]string{"1", "2"}, []string{"2", "3"})
-	log.Println(strings)
 }
 func TestName(t *testing.T) {
 	var a = "ABCD"
