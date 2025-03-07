eb05cd5d7e23481fe7b5458b9cd127b28758e22a
何其龙 <heqilong.hl@bytedance.com>
2025-03-07 16:38:14 +0800

    feat：添加函数 MergeAndDeduplicateStrings

--- f/str.go
+++ f/str.go
@@ -9,3 +9,28 @@ func ToString(obj interface{}) string {
 	str, _ := sonic.MarshalString(obj)
 	return str
 }
+
+// MergeAndDeduplicateStrings 合并两个字符串数组并去除重复元素
+func MergeAndDeduplicateStrings(arr1, arr2 []string) []string {
+	// 创建一个 map 用于存储已经出现过的元素
+	uniqueMap := make(map[string]bool)
+	var result []string
+
+	// 遍历第一个数组
+	for _, str := range arr1 {
+		if !uniqueMap[str] {
+			uniqueMap[str] = true
+			result = append(result, str)
+		}
+	}
+
+	// 遍历第二个数组
+	for _, str := range arr2 {
+		if !uniqueMap[str] {
+			uniqueMap[str] = true
+			result = append(result, str)
+		}
+	}
+
+	return result
+}
--- test/fData_test.go
+++ test/fData_test.go
@@ -1,10 +1,17 @@
 package test
 
 import (
+	"log"
 	"testing"
+
+	"github.com/zhenjunhl/fa-tian-xiang-di/f"
 )
 
 func TestGetTimestampHoursAgo(t *testing.T) {
 	// ago := f.GetTimestampHoursAgo(1)
 	// fmt.Println(ago)
+}
+func TestMergeAndDeduplicateStrings(t *testing.T) {
+	strings := f.MergeAndDeduplicateStrings([]string{"1", "2"}, []string{"2", "3"})
+	log.Println(strings)
 }
