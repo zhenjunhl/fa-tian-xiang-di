c3615cabb7e0582f1747456416da67dd748b59a0
何其龙 <heqilong.hl@bytedance.com>
2025-03-07 15:47:49 +0800

    feat：整理目录结构

--- test/fData_test.go
+++ test/fData_test.go
@@ -1,13 +1,10 @@
 package test
 
 import (
-	"fmt"
 	"testing"
-
-	"github.com/zhenjunhl/fa-tian-xiang-di/f"
 )
 
 func TestGetTimestampHoursAgo(t *testing.T) {
-	ago := f.GetTimestampHoursAgo(1)
-	fmt.Println(ago)
+	// ago := f.GetTimestampHoursAgo(1)
+	// fmt.Println(ago)
 }
